bd1f37e91992226eaafb97ba50f997ba354be34b
Gartlen <valentina.vargas.carvajal10@gmail.com>
2023-07-14 11:59:26 -0500

    subo curso react, ultima actualizacion local host

--- src/App.js
+++ src/App.js
@@ -6,23 +6,87 @@ import { CreateTodoButton } from './CreateTodoButton';
 import { TodoItem } from './TodoItem';
 import React from 'react'
 
-const defaultTodos = [
-  { text: 'Aprender JavaScript', completed: false },
-  { text: 'Hacer una pagina web', completed: false },
-  { text: 'Bañarme', completed: true },
-  { text: 'Jugar Minecraft', completed: false },
-];
+// const defaultTodos = [
+//   { text: 'Aprender JavaScript', completed: false },
+//   { text: 'Hacer una pagina web', completed: false },
+//   { text: 'Bañarme', completed: true },
+//   { text: 'Jugar Minecraft', completed: true },
+  
+// ];
+ 
+ 
+//  localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+//  localStorage.removeItem(''TODOS_V1)
+
 
 function App() {
+
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  
+  let parcedTodos;
+  
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parcedTodos = [];
+  } else {
+
+    parcedTodos = JSON.parse(localStorageTodos);
+  }
+
+
+  const [todos, setTodos]= React.useState(parcedTodos)
+  const [searchValue, setSearchValue]= React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todoText.includes(searchText);
+  });
+
+  const saveTodos = (newTodos) => {
+
+    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
+
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos);
+  };
+   
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    );
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+
   return (
     
     <>
-      <TodoCounter completed={16} total={25} />
-      <TodoSearch/>
+      <TodoCounter
+      completed={completedTodos} total={totalTodos} />
+      <TodoSearch
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+      />
 
       <TodoList>
-      {defaultTodos.map(todo => (
-        <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+      {searchedTodos.map(todo => (
+        <TodoItem key={todo.text} text={todo.text} completed={todo.completed} 
+        onComplete={() => completeTodo(todo.text)}
+        onDelete={() => deleteTodo(todo.text)}
+        />
       ))}
       </TodoList>
 
---
+++ src/TodoIcon.js
@@ -0,0 +1,23 @@
+import { ReactComponent as CheckSVG } from './CheckBlack.svg';
+import { ReactComponent as DeleteSVG } from './DeleteBlack.svg';
+import './TodoIcon.css'
+
+const iconTypes = {
+    "check": (color) => <CheckSVG className='Icon-svg' fill={color}/>,
+    "delete":(color) =>  <DeleteSVG className='Icon-svg' fill={color}/>,
+};
+
+function TodoIcon ({type, color, onClick }){
+
+return (
+    <span className={`Icon-container Icon-container-${type}`}
+    onClick={onClick}
+    >
+        {iconTypes[type](color)}
+        
+    </span>
+);
+
+}
+
+export {TodoIcon};
